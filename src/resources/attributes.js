434d2dbea661e1dee94a393efdae02074323aae7
omabytes <321.trebor@gmail.com>
2018-10-26 10:12:40 +0100

    generated attributes to avoid undefined errors while deving

--- src/resources/attributes.js
+++ src/resources/attributes.js
@@ -5,5 +5,40 @@ export const attributes = {
         "CharismaProficiency3",
         "CharismaProficiency4",
         "CharismaProficiency5"
+    ],
+    Constitution: [
+        "ConstitutionProficiency1",
+        "ConstitutionProficiency2",
+        "ConstitutionProficiency3",
+        "ConstitutionProficiency4",
+        "ConstitutionProficiency5"
+    ],
+    Dexterity: [
+        "DexterityProficiency1",
+        "DexterityProficiency2",
+        "DexterityProficiency3",
+        "DexterityProficiency4",
+        "DexterityProficiency5"
+    ],
+    Intelligence: [
+        "IntelligenceProficiency1",
+        "IntelligenceProficiency2",
+        "IntelligenceProficiency3",
+        "IntelligenceProficiency4",
+        "IntelligenceProficiency5"
+    ],
+    Strength: [
+        "StrengthProficiency1",
+        "StrengthProficiency2",
+        "StrengthProficiency3",
+        "StrengthProficiency4",
+        "StrengthProficiency5"
+    ],
+    Wisdom: [
+        "WisdomProficiency1",
+        "WisdomProficiency2",
+        "WisdomProficiency3",
+        "WisdomProficiency4",
+        "WisdomProficiency5"
     ]
 }
