a6fe6ce539f795ddbf5446ee79134eb4be564566
omabytes <321.trebor@gmail.com>
2018-10-29 12:06:30 +0000

    extract updateScore call to function

--- src/containers/BarContainer.jsx
+++ src/containers/BarContainer.jsx
@@ -4,13 +4,21 @@ import { connect } from 'react-redux'
 import { updateScore } from '../actions/updateScore'
 import Bar from '../components/Bar';
 
+function updateScores(updateScore, skillName, newScore) {
+    updateScore(skillName, newScore)
+}
+
 class BarContainer extends Component {
     render() {
         return (
             <div className="bar-container">
                 <Bar
                     score={this.props[this.props.skillName]}
-                    updateScore={(newScore) => this.props.updateScore(this.props.skillName, newScore)}
+                    updateScore={(newScore) => updateScores(
+                        this.props.updateScore,
+                        this.props.skillName,
+                        newScore
+                    )}
                     disabled={this.props.disabled}
                 />
             </div>
