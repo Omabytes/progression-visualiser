eda8790b3e51915380644b63e2cc833532fdfec2
omabytes <321.trebor@gmail.com>
2018-10-30 16:15:39 +0000

    attributes changed to DnD

--- src/resources/attributes.js
+++ src/resources/attributes.js
@@ -1,135 +1,102 @@
 export const attributes = {
     Cleric: {
        Charisma: [
-            "CharismaProficiency1",
-            "CharismaProficiency2",
-            "CharismaProficiency3",
-            "CharismaProficiency4",
-            "CharismaProficiency5"
+            "Deception",
+            "Intimidation",
+            "Performance",
+            "Persuasion"
         ],
         Constitution: [
-            "ConstitutionProficiency1",
-            "ConstitutionProficiency2",
-            "ConstitutionProficiency3",
-            "ConstitutionProficiency4",
-            "ConstitutionProficiency5"
+            "Not-being-poisonedness"
         ],
         Dexterity: [
-            "DexterityProficiency1",
-            "DexterityProficiency2",
-            "DexterityProficiency3",
-            "DexterityProficiency4",
-            "DexterityProficiency5"
+            "Acrobatics",
+            "Sleight of Hand",
+            "Stealth"
         ],
         Intelligence: [
-            "IntelligenceProficiency1",
-            "IntelligenceProficiency2",
-            "IntelligenceProficiency3",
-            "IntelligenceProficiency4",
-            "IntelligenceProficiency5"
+            "Arcana",
+            "History",
+            "Investigation",
+            "Nature",
+            "Religion"
         ],
         Strength: [
-            "StrengthProficiency1",
-            "StrengthProficiency2",
-            "StrengthProficiency3",
-            "StrengthProficiency4",
-            "StrengthProficiency5"
+            "Athletics"
         ],
         Wisdom: [
-            "WisdomProficiency1",
-            "WisdomProficiency2",
-            "WisdomProficiency3",
-            "WisdomProficiency4",
-            "WisdomProficiency5"
-        ] 
+            "Animal Handling",
+            "Insight",
+            "Medicine",
+            "Perception",
+            "Survival"
+        ]
     },
     Druid: {
         Charisma: [
-             "CharismaProficiency1",
-             "CharismaProficiency2",
-             "CharismaProficiency3",
-             "CharismaProficiency4",
-             "CharismaProficiency5"
-         ],
-         Constitution: [
-             "ConstitutionProficiency1",
-             "ConstitutionProficiency2",
-             "ConstitutionProficiency3",
-             "ConstitutionProficiency4",
-             "ConstitutionProficiency5"
-         ],
-         Dexterity: [
-             "DexterityProficiency1",
-             "DexterityProficiency2",
-             "DexterityProficiency3",
-             "DexterityProficiency4",
-             "DexterityProficiency5"
-         ],
-         Intelligence: [
-             "IntelligenceProficiency1",
-             "IntelligenceProficiency2",
-             "IntelligenceProficiency3",
-             "IntelligenceProficiency4",
-             "IntelligenceProficiency5"
-         ],
-         Strength: [
-             "StrengthProficiency1",
-             "StrengthProficiency2",
-             "StrengthProficiency3",
-             "StrengthProficiency4",
-             "StrengthProficiency5"
-         ],
-         Wisdom: [
-             "WisdomProficiency1",
-             "WisdomProficiency2",
-             "WisdomProficiency3",
-             "WisdomProficiency4",
-             "WisdomProficiency5"
-         ] 
+            "Deception",
+            "Intimidation",
+            "Performance",
+            "Persuasion"
+        ],
+        Constitution: [
+            "Not-being-poisonedness"
+        ],
+        Dexterity: [
+            "Acrobatics",
+            "Sleight of Hand",
+            "Stealth"
+        ],
+        Intelligence: [
+            "Arcana",
+            "History",
+            "Investigation",
+            "Nature",
+            "Religion"
+        ],
+        Strength: [
+            "Athletics"
+        ],
+        Wisdom: [
+            "Animal Handling",
+            "Insight",
+            "Medicine",
+            "Perception",
+            "Survival"
+        ]
      },
      Monk: {
         Charisma: [
-             "CharismaProficiency1",
-             "CharismaProficiency2",
-             "CharismaProficiency3",
-             "CharismaProficiency4",
-             "CharismaProficiency5"
-         ],
-         Constitution: [
-             "ConstitutionProficiency1",
-             "ConstitutionProficiency2",
-             "ConstitutionProficiency3",
-             "ConstitutionProficiency4",
-             "ConstitutionProficiency5"
-         ],
-         Dexterity: [
-             "DexterityProficiency1",
-             "DexterityProficiency2",
-             "DexterityProficiency3",
-             "DexterityProficiency4",
-             "DexterityProficiency5"
-         ],
-         Intelligence: [
-             "IntelligenceProficiency1",
-             "IntelligenceProficiency2",
-             "IntelligenceProficiency3",
-             "IntelligenceProficiency4",
-             "IntelligenceProficiency5"
-         ],
-         Strength: [
-             "StrengthProficiency1",
-             "StrengthProficiency2",
-             "StrengthProficiency3",
-             "StrengthProficiency4",
-             "StrengthProficiency5"
-         ],
-         Wisdom: [
-             "WisdomProficiency1",
-             "WisdomProficiency2",
-             "WisdomProficiency3",
-             "WisdomProficiency4",
-             "WisdomProficiency5"
-         ] 
+            "Deception",
+            "Intimidation",
+            "Performance",
+            "Persuasion"
+        ],
+        Constitution: [
+            "Not-being-poisonedness"
+        ],
+        Dexterity: [
+            "Acrobatics",
+            "Sleight of Hand",
+            "Stealth"
+        ],
+        Intelligence: [
+            "Arcana",
+            "History",
+            "Investigation",
+            "Nature",
+            "Religion"
+        ],
+        Strength: [
+            "Athletics"
+        ],
+        Wisdom: [
+            "Animal Handling",
+            "Insight",
+            "Medicine",
+            "Perception",
+            "Survival"
+        ]
      },
     
 }
