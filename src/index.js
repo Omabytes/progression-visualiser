9215fc2edddb5ab91f631b1350100a316c205778
omabytes <321.trebor@gmail.com>
2018-11-01 13:35:01 +0000

    save state in local storage

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import rootReducer from './reducers'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { loadState, saveState } from './localStorage'
+
+const persistedState = loadState()
 
 const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+    saveState(store.getState())
+})
 
 render(
     <Provider store={store}>
