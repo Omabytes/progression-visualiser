7bb13d4a106b32113b6cd50eab681d6274ea34bd
omabytes <321.trebor@gmail.com>
2018-11-02 11:04:04 +0000

    handle the attributes object changing

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { updateRole } from "../actions/updateRole";
 import SkillContainer from '../containers/SkillContainer'
 import SelectorContainer from '../containers/SelectorContainer'
 import { attributes } from '../resources/attributes'
@@ -13,13 +15,20 @@ const generateSkills = function(skillName) {
     />
 }
 
-const HomePage = ({ role }) => (
+const clearRole = (updateRole) => {
+    updateRole(null)
+    return null
+}
+
+let roles = Object.keys(attributes)
+
+const HomePage = ({ role, updateRole }) => (
     <div className="App">
         <header className="App-header">
-            <SelectorContainer roles={Object.keys(attributes)}/>
+            <SelectorContainer roles={roles}/>
         </header>
         <div className="skills">
-            {role === null ? null : Object.keys(attributes[role]).map(generateSkills)}
+            {roles.includes(role) ? Object.keys(attributes[role]).map(generateSkills) : clearRole(updateRole)}
         </div>
     </div>
 )
@@ -30,4 +39,8 @@ function mapStateToProps({ selectorReducer }) {
     }
 }
 
-export default connect(mapStateToProps)(HomePage)
+function mapDispatchToProps(dispatch) {
+    return { ...bindActionCreators({ updateRole }, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
