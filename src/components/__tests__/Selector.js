a692c1ffeedc6e94ae6f37f775d78556bc0fe799
omabytes <321.trebor@gmail.com>
2018-11-14 17:08:10 +0000

    Selector unit tests

--- src/components/__tests__/Selector.js
+++ src/components/__tests__/Selector.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import Selector from '../Selector'
+import Select from 'rc-select'
+
+const wrapper = shallow(<Selector roles={['Warlock', 'Monk']} />)
 
 it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<Selector />, div)
 })
+
+it('contains a Select component', () => {
+    expect(wrapper.find(Select).length).toEqual(1)
+})
+
+it('displays "Choose Role..." before role selection', () => {
+    expect(wrapper.find(Select).props().value).toEqual('Choose Role...')
+})
+
+it('creates Select options from props', () => {
+    expect(wrapper.find(Select.Option).length).toEqual(2)
+})
+
