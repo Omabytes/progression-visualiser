1933c17cd31d448792d8cd78a871799c5cb2211d
omabytes <321.trebor@gmail.com>
2018-11-16 11:28:41 +0000

    file name now with timestamp

--- src/fileDownloader.jsx
+++ src/fileDownloader.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import store from './store'
 
+const pad = (x) => {
+  return (x < 10 ? '0' : '') + x
+}
+
+const getFilename = () => {
+  let d = new Date()
+
+  return 'score-profile-' +
+    d.getFullYear() +
+    (pad(d.getMonth() + 1)) +
+    pad(d.getDate()) +
+    '-' +
+    pad(d.getHours()) +
+    pad(d.getMinutes()) +
+    pad(d.getSeconds())
+}
+
 const fd = (data) => {
     const blob = new Blob([data], { type: 'application/octet-stream' });
     const blobURL = window.URL.createObjectURL(blob);
     const tempLink = document.createElement('a');
     tempLink.style.display = 'none';
     tempLink.href = blobURL;
-    tempLink.setAttribute('download', 'score-profile');
+    tempLink.setAttribute('download', getFilename());
     document.body.appendChild(tempLink);
     tempLink.click();
     document.body.removeChild(tempLink);
